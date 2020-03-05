234d37c7f7d9a7379144da9357b701a3f363693e
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-03-05 17:48:06 +0545

    Defining the products display fields in AdminAddNewProductActivity

--- app/src/main/java/com/example/ecommerceapp/AdminAddNewProductActivity.java
+++ app/src/main/java/com/example/ecommerceapp/AdminAddNewProductActivity.java
@@ -2,12 +2,19 @@ package com.example.ecommerceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private Button AddNewProductButton;
+    private EditText InputProductName, InputProductDescription ,InputProductPrice;
+    private ImageView ProductImage;
 
 
     @Override
@@ -16,6 +23,14 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add_new_product);
 
         categoryName = getIntent().getExtras().get("category").toString();
+
+        AddNewProductButton = (Button)findViewById(R.id.Add_product);
+        InputProductName=(EditText)findViewById(R.id.product_name);
+        InputProductDescription=(EditText)findViewById(R.id.product_description);
+        InputProductPrice=(EditText)findViewById(R.id.product_price);
+        ProductImage=(ImageView)findViewById(R.id.product_image);
+
+
 
 
 
