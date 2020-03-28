8207ae7a82ad970b80aa7466b3851ae165dbac9e
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-03-28 14:44:56 +0545

    Database for Cart Activity added

--- app/src/main/java/com/example/ecommerceapp/ProductDetailsActivity.java
+++ app/src/main/java/com/example/ecommerceapp/ProductDetailsActivity.java
@@ -1,20 +1,31 @@
 package com.example.ecommerceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.example.ecommerceapp.Model.Products;
+import com.example.ecommerceapp.Prevalent.Prevalent;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
@@ -45,6 +56,65 @@ public class ProductDetailsActivity extends AppCompatActivity {
         productID = getIntent().getStringExtra("pid");
         getProductDetails(productID);
 
+        add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              addingToCartList();
+
+            }
+        });
+
+    }
+
+    private void addingToCartList() {
+        String saveCurrentTime, saveCurrentDate;
+        Calendar calForDate = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentDate.format(calForDate.getTime());
+
+       /**
+          Creating table for the CartList
+         **/
+
+       final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("Cart List");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("pid", productID);
+        cartMap.put("pname", productName.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberButton.getNumber());
+        cartMap.put("discount", "");
+
+        cartListRef.child("User View").child(Prevalent.currentOnlineUser.getPhone())
+                .child("Products").child(productID)
+                .updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            cartListRef.child("Admin View").child(Prevalent.currentOnlineUser.getPhone())
+                                    .child("Products").child(productID)
+                                    .updateChildren(cartMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(ProductDetailsActivity.this, "Added To Cart List", Toast.LENGTH_SHORT).show();
+                                                Intent intent = new Intent(ProductDetailsActivity.this,HomeActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 
     private void getProductDetails(String productID) {
